d9a382ae4ae05a188313c2b76416b5228baaf719
jun9ho <wjdgh9806@korea.ac.kr>
2025-09-25 10:52:31 +0900

    app page 수정

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,18 @@
 import Image from "next/image";
 
-export default function Home() {
+async function getResumeInfo() {
+  const res = await fetch('https://raw.githubusercontent.com/jun9ho/first-deploy/refs/heads/0.3/general_info/service/resume_general_info_service.json');
+  // API 응답이 성공적인지 확인
+  if (!res.ok) {
+    // 응답이 실패하면 오류를 던져 Next.js가 오류 페이지를 보여주도록 함
+    throw new Error('Failed to fetch data');
+  }
+  return res.json();
+}
+
+
+export default async function Home() {
+  const data = await getResumeInfo();
   return (
     <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
@@ -16,9 +28,15 @@ export default function Home() {
           <li className="mb-2 tracking-[-.01em]">
             안녕하세요{" "}
             <code className="bg-black/[.05] dark:bg-white/[.06] font-mono font-semibold px-1 py-0.5 rounded">
-              황정호 입니다.
+              {data.name}
             </code>
-            .
+            입니다.
+            <br /> 
+            제 GitHub 주소는
+            <code className="bg-black/[.05] dark:bg-white/[.06] font-mono font-semibold px-1 py-0.5 rounded">
+            {data.github}
+            </code>
+            입니다.
           </li>
           <li className="tracking-[-.01em]">
             반갑습니다.
