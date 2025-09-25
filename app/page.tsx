0567001a2b468ee7f4d4ab02d24eb5dcb462ee5b
jun9ho <wjdgh9806@korea.ac.kr>
2025-09-25 11:01:16 +0900

    프로젝트 정보 서비스 - 프로젝트 이름, 설명,,, 등등 입력

--- app/page.tsx
+++ app/page.tsx
@@ -10,9 +10,20 @@ async function getResumeInfo() {
   return res.json();
 }
 
+async function getPortfolioInfo() {
+  const res = await fetch('https://raw.githubusercontent.com/jun9ho/first-deploy/refs/heads/0.3/general_info/service/resume_portfolio_service.json');
+  // API 응답이 성공적인지 확인
+  if (!res.ok) {
+    // 응답이 실패하면 오류를 던져 Next.js가 오류 페이지를 보여주도록 함
+    throw new Error('Failed to fetch data');
+  }
+  return res.json();
+}
+
 
 export default async function Home() {
-  const data = await getResumeInfo();
+  const resumeData = await getResumeInfo();
+  const portfolioData = await getPortfolioInfo();
   return (
     <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
@@ -28,15 +39,28 @@ export default async function Home() {
           <li className="mb-2 tracking-[-.01em]">
             안녕하세요{" "}
             <code className="bg-black/[.05] dark:bg-white/[.06] font-mono font-semibold px-1 py-0.5 rounded">
-              {data.name}
+              {resumeData.name}
             </code>
             입니다.
             <br /> 
             제 GitHub 주소는
             <code className="bg-black/[.05] dark:bg-white/[.06] font-mono font-semibold px-1 py-0.5 rounded">
-            {data.github}
+            {resumeData.github}
             </code>
             입니다.
+            <br /> 
+            제 첫 프로젝트 이름은
+            <code className="bg-black/[.05] dark:bg-white/[.06] font-mono font-semibold px-1 py-0.5 rounded">
+            {portfolioData.project_name}
+            </code>
+            입니다.
+            <br />
+            이 프로젝트는
+            <code className="bg-black/[.05] dark:bg-white/[.06] font-mono font-semibold px-1 py-0.5 rounded">
+            {portfolioData.project_info}
+            </code>
+            입니다.
+            <br />
           </li>
           <li className="tracking-[-.01em]">
             반갑습니다.
